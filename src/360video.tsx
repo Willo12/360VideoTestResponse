5f484589b7438336fadd3744964828cd0ac3c19e
Willo12 <115796146+Willo12@users.noreply.github.com>
2024-03-20 12:05:11 +0000

    made video go center screen

--- src/360video.tsx
+++ src/360video.tsx
@@ -6,7 +6,7 @@ const Video = ({ src }: VideoProps) => {
   return (
     <>
       <div className="flex mt-2 justify-center items-center">
-        <div className="shadow-md">
+        <div className="shadow-md flex justify-center">
           <iframe
             className="w-[330px] h-[450px] lg:w-[720px] lg:h-[400px] 2xl:w-[1280px] 2xl:h-[720px]"
             src="https://www.youtube.com/embed/Hl2lZl-N98U?si=l3MkjKa2Mj1UqJhj"
