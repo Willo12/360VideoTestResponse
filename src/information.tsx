471d0848555439a6930a22ca9c7810e79f54a6f1
Willo12 <115796146+Willo12@users.noreply.github.com>
2024-03-21 12:11:44 +0000

    add border to site info

--- src/information.tsx
+++ src/information.tsx
@@ -7,13 +7,15 @@ type InfoProps = {
 const Info = ({ title, explanation, madeWith }: InfoProps) => {
   return (
     <div className="flex justify-center items-center mt-4 mx-4">
-      <h1 className="text-2xl lg:xl 2xl:text-6xl block ">
-        {title}
-        <div className="">
-          <p className="2xl:text-3xl mt-6 lg:mt-6">{explanation}.</p>
-          <p className="2xl:text-3xl mt-10 lg:mt-2">{madeWith}</p>
-        </div>
-      </h1>
+      <div className="bg-gray-400 rounded-md shadow-md">
+        <h1 className="text-2xl lg:xl 2xl:text-6xl mx-4 my-2  block">
+          {title}
+          <div className="mb-10 mx-4">
+            <p className="2xl:text-3xl mt-6 lg:mt-6">{explanation}.</p>
+            <p className="2xl:text-3xl mt-10 lg:mt-2">{madeWith}</p>
+          </div>
+        </h1>
+      </div>
     </div>
   );
 };
